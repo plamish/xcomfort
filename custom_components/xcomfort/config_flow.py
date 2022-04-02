d5c72d4eebd1044fd5f4a7d919f9daf77ddb2cab
plamish <52619470+plamish@users.noreply.github.com>
2022-04-02 17:19:07 +0200

    1.3.3

--- custom_components/xcomfort/config_flow.py
+++ custom_components/xcomfort/config_flow.py
@@ -9,7 +9,7 @@ from .const import DOMAIN
 DATA_SCHEMA = vol.Schema(
     {
         vol.Required("url", default="http://10.0.0.20"): str,
-        vol.Required("username", default="admin"): str,
+        vol.Required("username", default="admin1"): str,
         vol.Required("password",default=""): str,
         vol.Required("zone",default="hz_3"): str,
         vol.Required("scan_interval", default=5): int,
@@ -38,8 +38,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     user_input["url"], raise_on_progress=False
                 )
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(
-                    title="xcomfort", data=user_input,
+            return self.async_create_entry( title="xcomfort",  data=user_input,
                 )
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors,
